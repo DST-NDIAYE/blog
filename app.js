e4db1e2b48bf164013ae80cf709dc80fccf3a742
Diamou Serigne Touba Ndiaye <dstndiaye@gmail.com>
2022-08-20 15:15:34 +0000

    aficher un seul article

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/article/:articleId', indexRouter);
 
 
 // catch 404 and forward to error handler
