6af2e319dff93685b00c2461f2104dd0c08e238c
Diamou Serigne Touba Ndiaye <dstndiaye@gmail.com>
2022-08-20 12:50:29 +0000

    recuperation d'un seul article

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var article = require('../models/article.model.ts')
+var articles = require('../models/article.model.ts')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  article.find()
+  articles.find()
   .then( articleRecu => 
     res.render('index', { title: 'DST NDAYE' , 'articles' : articleRecu}))
   .catch(err => console.log(err))
 
 });
+
+
+router.get('/article/:articleId', (req, res) => {
+    articleId  = req.params.articleId ;
+
+    articles.findOne({_id: articleId})
+    .then( articleRecu => 
+      console.log(articleRecu)
+      // res.render('index', { title: 'DST NDAYE' , 'articles' : articleRecu })
+    )
+    .catch(err => console.log(err))
+  
+
+    console.log(articleId);
+})
 
 module.exports = router;
