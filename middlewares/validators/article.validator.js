827254b42f062e37f9369099243a5747d98dda35
Diamou Serigne Touba Ndiaye <dstndiaye@gmail.com>
2022-08-25 22:17:46 +0000

    inplementation du validator

--- middlewares/validators/article.validator.js
+++ middlewares/validators/article.validator.js
@@ -1,1 +1,22 @@
 const { Validator } = require('node-input-validator');
+
+
+const articleValidator = (res, res, next) => {
+    const v = new Validator(req.body, {
+        title: 'require',
+        categorie: 'required',
+        content: 'required',
+        image: 'required'
+    });
+
+    v.check().then(( matched ) =>{
+        if (!matched) {
+            // error
+            req.flash('errorFormValidator', v.errors);
+            return res.redirect('/add-article');
+        }
+        next();
+    });
+}
+
+module.exports = articleValidator ;
